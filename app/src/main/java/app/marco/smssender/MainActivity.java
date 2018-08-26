1a6a65c1b707521cbf6d348ff5da57bb7dd4d5cf
marcoSource <gammma7869@gmail.com>
2018-08-26 23:59:20 +0100

    FristBuild

---
+++ app/src/main/java/app/marco/smssender/MainActivity.java
@@ -0,0 +1,55 @@
+package app.marco.smssender;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mPhoneNumber, mBody, mNumberOfSms;
+    private ProgressBar mSendingProgress;
+    private Button mSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS},1);
+
+        mPhoneNumber = (EditText) findViewById(R.id.phoneNumber);
+        mBody = (EditText) findViewById(R.id.body);
+        mNumberOfSms = (EditText) findViewById(R.id.numberOfSms);
+        mSend = (Button) findViewById(R.id.send);
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+    }
+
+    private void sendMessage(){
+        int numberOfSms = Integer.parseInt(mNumberOfSms.getText().toString());
+        for(int i = 0; i < numberOfSms; i++){
+            if(i >= numberOfSms){
+                SmsManager sms = SmsManager.getDefault();
+                sms.sendTextMessage(mPhoneNumber.getText().toString(), null, "Successfully send " + mNumberOfSms.getText().toString() + " messages!" , null, null);
+                Toast.makeText(this, "Successfully send " + mNumberOfSms.getText().toString() + " messages to " + mPhoneNumber.getText().toString() + "!", Toast.LENGTH_LONG).show();
+            } else {
+                SmsManager sms = SmsManager.getDefault();
+                sms.sendTextMessage(mPhoneNumber.getText().toString(), null, mBody.getText().toString(), null, null);
+                Toast.makeText(this, "Sms " + i + " from " + numberOfSms + " sended.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
